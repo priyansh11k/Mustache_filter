e6ba91d7a2622ca46ec788f931d6c31e83abde12
priyansh11k <80451561+priyansh11k@users.noreply.github.com>
2021-12-03 11:33:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-function preload(){
+nosex = "";
+nosey = "";
 
+function preload(){
+    img = loadImage("https://i.postimg.cc/hvGhYM1C/A-Mustache-Looks-Like-a-Big-Hairy-Smile-removebg-preview.png");
 }
 
 function setup(){
     canvas = createCanvas(300,300);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(300,300);
-    video.hide();
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPose);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPose);
 }
 
 function snap(){
-save("filter_clownnose.png");
+save("filter_MUSTACEHE.png");
 }
 
 function draw(){
     image(video,0,0,300,300);
+    image(img,nosex,nosey,40,40) ;
 }
 
 function modelLoaded(){
@@ -25,13 +29,16 @@ function modelLoaded(){
     }
     
     function gotPose(results){
-    
-    if (results.length > 0) {
-     console.log(results);  
-     console.log("nose x = " + results[0].pose.nose.x);
-     console.log("nose y =" + results[0].pose.nose.y);
+        if (results.length > 0) {
+            console.log(results);  
+            nosex = results[0].pose.nose.x-20;
+            nosey = results[0].pose.nose.y+15 ; 
+            console.log("nose x = " + nosex);
+            console.log("nose y =" + nosey);
+           
+           }
     
     }
-    }
+    
     
     
